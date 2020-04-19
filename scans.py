74a6272504393d77aecc7194923fa28046c965c9
Rafe Hart <rafael.hart9@gmail.com>
2020-04-19 16:13:36 +1000

    Don't repeat scans you've already done

--- scans.py
+++ scans.py
@@ -55,57 +55,60 @@ def run_amass(target, amass_config):
     print_bold_green("Running Amass to find sub-domains")
 
     output_file = target + "/" + target + ".amass.txt"
-    if os.path.exists(output_file):
-        print_yellow("Moving previous amass results to " + output_file + ".bak")
-        cmdstring = "mv " + output_file + " " + output_file + ".bak"
+    if not os.path.exists(output_file):
+        if os.path.exists(amass_config):
+            print_green("Using Amass config " + amass_config)
+            cmdstring = "amass enum -config " + amass_config + " -brute -d " + target + " -o " + \
+                        target + "/" + target + ".amass.txt"
+        else:
+            print_grey("Not using config.ini")
+            cmdstring = "amass enum -brute -d " + target + " -o " + output_file
         os.system(cmdstring)
-    if os.path.exists(amass_config):
-        print_green("Using Amass config " + amass_config)
-        cmdstring = "amass enum -config " + amass_config + " -brute -d " + target + " -o " + \
-                    target + "/" + target + ".amass.txt"
     else:
-        print_grey("Not using config.ini")
-        cmdstring = "amass enum -brute -d " + target + " -o " + output_file
-    os.system(cmdstring)
+        print_yellow("Previous amass results exist. Skipping.")
     count_results('Amass', output_file)
     # need to define and add -min-for-recursive 3
-    # abort script if amass exits
 
 def run_assetfinder(target, FB_APP_ID, FB_APP_SECRET, VT_API_KEY, SPYSE_API_TOKEN):
     ''' Runs Assetfinder after exporting environment variables '''
     print_bold_green("Running Assetfinder to find sub-domains")
-
-    if FB_APP_ID: os.environ["FB_APP_ID"] = FB_APP_ID
-    if FB_APP_SECRET: os.environ["FB_APP_SECRET"] = FB_APP_SECRET
-    if VT_API_KEY: os.environ["VT_API_KEY"] = VT_API_KEY
-    if SPYSE_API_TOKEN: os.environ["SPYSE_API_TOKEN"] = SPYSE_API_TOKEN
-
     output_file = target + "/" + target + ".assetfinder.txt"
-    if os.path.exists(output_file):
-        print_yellow("Moving previous assetfinder results to " + output_file + ".bak")
-        cmdstring = "mv " + output_file + " " + output_file + ".bak"
+
+    if not os.path.exists(output_file):
+        if FB_APP_ID: os.environ["FB_APP_ID"] = FB_APP_ID
+        if FB_APP_SECRET: os.environ["FB_APP_SECRET"] = FB_APP_SECRET
+        if VT_API_KEY: os.environ["VT_API_KEY"] = VT_API_KEY
+        if SPYSE_API_TOKEN: os.environ["SPYSE_API_TOKEN"] = SPYSE_API_TOKEN
+
+        cmdstring = "assetfinder -subs-only " + target + " > " + output_file
         os.system(cmdstring)
-    cmdstring = "assetfinder -subs-only " + target + " > " + output_file
-    os.system(cmdstring)
+    else:
+        print_yellow("Previous Assetfinder results exist. Skipping")
     count_results('Assetfinder', output_file)
 
 def run_subfinder(target):
     ''' Runs Subfinder to find subdomains '''
     print_bold_green("Running Subfinder to find sub-domains")
-
     output_file = target + "/" + target + ".subfinder.txt"
-    cmdstring = "subfinder -d " + target + " -o " + output_file
-    os.system(cmdstring)
+
+    if not os.path.exists(output_file):
+        cmdstring = "subfinder -d " + target + " -o " + output_file
+        os.system(cmdstring)
+    else:
+        print_yellow("Previous Subfinder results exist. Skipping")
     count_results('Subfinder', output_file)
 
 def run_dnsbuffer(target):
     ''' Gets subdomains from Rapid7 '''
     print_bold_green("Running DNS Buffer to find sub-domains")
-
     output_file = target + "/" + target + ".bufferover.txt"
-    cmdstring = "curl -s https://dns.bufferover.run/dns?q=." + target + " | jq -r .FDNS_A[] | " \
-                "cut -d',' -f2 > " + output_file
-    os.system(cmdstring)
+
+    if not os.path.exists(output_file):
+        cmdstring = "curl -s https://dns.bufferover.run/dns?q=." + target + " | jq -r .FDNS_A[] | " \
+                    "cut -d',' -f2 > " + output_file
+        os.system(cmdstring)
+    else:
+        print_yellow("Previous dnsbuffer results exist. Skipping")
     count_results('DNSBuffer', output_file)
 
 def combine_subdomain_results(target):
@@ -123,8 +126,12 @@ def run_dnsgen_and_massdns(target, massdns_resolvers):
 
     combined_domain_file = target + "/" + target + ".combined.txt"
     output_file = target + "/" + target + ".massdns.txt"
-    cmdstring = "cat " + combined_domain_file + " | dnsgen - | massdns -r " + massdns_resolvers + " -t A -o S -w " + output_file
-    os.system(cmdstring)
+
+    if not os.path.exists(output_file):
+        cmdstring = "cat " + combined_domain_file + " | dnsgen - | massdns -r " + massdns_resolvers + " -t A -o S -w " + output_file
+        os.system(cmdstring)
+    else:
+        print_yellow("Previous dnsgen | massdns results exist. Skipping")
     count_results('dnsgen | massdns', output_file)
 
 def finalize_subdomain_results(target):
@@ -134,7 +141,6 @@ def finalize_subdomain_results(target):
     cmdstring = "sort " + target + "/" + target +".massdns.txt | awk '{print $1}' | sed 's/\.$//' | uniq > " + output_file
     os.system(cmdstring)
     count_results('Final Subdomains', output_file)
-
 
 def print_results_summary(target):
     print_bold_green("Summary of results")
