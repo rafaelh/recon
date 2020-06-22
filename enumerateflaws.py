b0af46fc7e2ac50637616c19cd700cd84db6525c
Rafe Hart <rafael.hart9@gmail.com>
2020-06-22 21:34:55 +1000

    Consolidating colours

--- enumerateflaws.py
+++ enumerateflaws.py
@@ -15,27 +15,27 @@ def show_dalfox_results(infile):
 
 def look_for_xss(xsshunter_domain, custom_xss_payloads, target, infile, outfile):
     ''' Look for XSS '''
-    print_bold_green("Looking for XSS")
+    print_message("green", "Looking for XSS")
     if not os.path.exists(target + "/" + outfile):
-        cmdstring = "cat " + target + "/" + infile + " | dalfox pipe --multicast -o " + target + "/" + outfile
+        cmdstring = "cat " + target + "/" + infile + " | dalfox pipe -o " + target + "/" + outfile
         if xsshunter_domain: cmdstring += " -b " + xsshunter_domain
         if custom_xss_payloads: cmdstring += " --custom-payload " + custom_xss_payloads
         os.system(cmdstring)
     else:
-        print_yellow("Previous dalfox results exist. Skipping.")
+        print_message("yellow", "Previous dalfox results exist. Skipping.")
     show_dalfox_results(target + "/" + outfile)
 
 def look_for_sqli(target, infile, outfile):
     ''' Look for SQLi '''
-    print_bold_green("Looking for SQLi")
+    print_message("green", "Looking for SQLi")
     if not os.path.exists(target + "/" + outfile):
         with open(target + "/" + infile, 'r') as rawlinksfile:
             lines = 0
             count = 0
             for line in rawlinksfile: lines += 1
             for line in rawlinksfile:
-                print_green("Testing " + count + "/" + lines + " for SQLi")
+                print_message("green", "Testing " + count + "/" + lines + " for SQLi")
                 cmdstring = "dsss.py -u " + line + "/" + infile + " >> " + target + "/" + outfile
                 os.system(cmdstring)
     else:
-        print_yellow("Previous SQLi results exist. Skipping.")
+        print_message("yellow", "Previous SQLi results exist. Skipping.")
